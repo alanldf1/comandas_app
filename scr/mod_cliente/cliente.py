762913430a6a09a9bb49e91fac27e5e7d41d73f2
Alan de Souza <alan_souza_nonijana@hotmail.com.br>
2023-11-14 18:44:16 -0300

    cadastro, edição e exclusão

--- scr/mod_cliente/cliente.py
+++ scr/mod_cliente/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 import requests
 from settings import HEADERS_API, ENDPOINT_CLIENTE
 
@@ -86,6 +86,8 @@ def edit():
 
         # monta o JSON para envio a API
         payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'telefone': telefone, 'compra_fiado': compra_fiado, 'dia_fiado': dia_fiado, 'senha': senha}
+        
+        print(id_cliente);
             
         # executa o verbo PUT da API e armazena seu retorno
         response = requests.put(ENDPOINT_CLIENTE + id_cliente, headers=HEADERS_API, json=payload)
@@ -95,6 +97,23 @@ def edit():
         return redirect(url_for('cliente.formListaCliente', msg=result[0]))
     except Exception as e:
         return render_template('formListaCliente.html', msgErro=e.args[0])
+    
+@bp_cliente.route('/delete', methods=['POST'])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id_cliente']
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_CLIENTE + id_cliente, headers=HEADERS_API)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        # return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+        # return render_template('formListaFuncionario.html',
+        return jsonify(erro=True, msgErro=e.args[0])
+
 
 '''
 Rota antiga de app...
--- scr/mod_produto/produto.py
+++ scr/mod_produto/produto.py
@@ -1,30 +1,43 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-import requests 
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+import requests
 from settings import HEADERS_API, ENDPOINT_PRODUTO
 import base64
 
-bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
+bp_produto = Blueprint("produto", __name__, url_prefix="/produto", template_folder="templates")
 
-''' rotas dos formulários '''
-# @bp_produto.route('/')
-# def formListaProduto():
-#     return render_template('formListaProduto.html'), 200
+""" rotas dos formulários """
 
-@bp_produto.route('/', methods=['GET', 'POST'])
+@bp_produto.route("/", methods=["GET", "POST"])
 def formListaProduto():
     try:
         response = requests.get(ENDPOINT_PRODUTO, headers=HEADERS_API)
         result = response.json()
-        if (response.status_code != 200):
+        if response.status_code != 200:
             raise Exception(result[0])
-        return render_template('formListaProduto.html', result=result[0])
+        return render_template("formListaProduto.html", result=result[0])
     except Exception as e:
-        return render_template('formListaProduto.html', msgErro=e.args[0])
+        return render_template("formListaProduto.html", msgErro=e.args[0])
 
 
-@bp_produto.route('/cadastro/', methods=['GET'])
+@bp_produto.route("/cadastro/", methods=["GET"])
 def formProduto():
-    return render_template('formProduto.html')
+    return render_template("formProduto.html")
+
+@bp_produto.route("/form-edit-produto", methods=["POST"])
+def formEditProduto():
+    try:
+        # ID enviado via FORM
+        id_produto = request.form["id"]
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if response.status_code != 200:
+            raise Exception(result[0])
+        # renderiza o form passando os dados retornados
+        return render_template("formProduto.html", result=result[0])
+    except Exception as e:
+        return render_template("formListaProduto.html", msgErro=e.args[0])
 
 
 @bp_produto.route('/insert', methods=['POST'])
@@ -34,12 +47,13 @@ def insert():
         id_produto = request.form['id']
         nome = request.form['nome']
         descricao = request.form['descricao']
-        valor_unitario = request.form['valor-unitario']
-        valor_unitario = valor_unitario.replace("R$", "").replace(",", ".");
-        valor_unitario = float(valor_unitario)
-        
+        valor_unitario  = request.form["valor-unitario"]
+        valor_unitario  = valor_unitario.replace("R$", "").replace(",", ".")
+        valor_unitario  = float(valor_unitario)
+
         # Converte a foto em base64
-        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+        foto = ("data:" + request.files["foto"].content_type + ";base64," + str(base64.b64encode(request.files["foto"].read()), "utf-8"))
+
 
         # monta o JSON para envio a API
         payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
@@ -47,61 +61,83 @@ def insert():
         # executa o verbo POST da API e armazena seu retorno
         response = requests.post(ENDPOINT_PRODUTO, headers=HEADERS_API, json=payload)
         result = response.json()
+                
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
         
         #return render_template('formListaFuncionario.html', msg=result[0])
         return redirect(url_for('produto.formListaProduto', msg=result[0]))
-    
+    except KeyError as e:
+        return render_template('formListaProduto.html', msgErro=f'Chave ausente: {e}')
+    except ValueError as e:
+        return render_template('formListaProduto.html', msgErro=f'Valor inválido: {e}')
     except Exception as e:
-        return render_template('formListaProduto.html', msgErro=e.args[0])
-
-
-@bp_produto.route("/form-edit-produto", methods=['POST'])
-def formEditProduto():
-    try:
-        # ID enviado via FORM
-        id_produto = request.form['id']
-        # executa o verbo GET da API buscando somente o funcionário selecionado,
-        # obtendo o JSON do retorno
-        response = requests.get(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
-        result = response.json()
-        if (response.status_code != 200):
-            raise Exception(result[0])
-        # renderiza o form passando os dados retornados
-        return render_template('formProduto.html', result=result[0])
-    except Exception as e:
-        return render_template('formListaProduto.html', msgErro=e.args[0])
+        return render_template('formListaProduto.html', msgErro=str(e))
+    # except Exception as e:
+    #     return render_template('formListaProduto.html', msgErro=e.args[0])
     
-@bp_produto.route('/edit', methods=['POST'])
+@bp_produto.route("/edit", methods=["POST"])
 def edit():
     try:
         # dados enviados via FORM
-        id_produto = request.form['id']
-        nome = request.form['nome']
-        descricao = request.form['descricao']
-        valor_unitario = request.form['valor-unitario']
-        valor_unitario = valor_unitario.replace("R$", "").replace(",", ".");
+        id_produto = request.form["id"]
+        nome = request.form["nome"]
+        descricao = request.form["descricao"]
+        valor_unitario = request.form["valor-unitario"]
+        valor_unitario = valor_unitario.replace("R$", "").replace(",", ".")
         valor_unitario = float(valor_unitario)
-          
         # Converte a foto em base64
-        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
-
-        # monta o JSON para envio a API
-        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
-        
+        if request.files['foto'].filename!= '' and request.files['foto'].filename is not None:
+            foto = (
+                "data:"
+                + request.files["foto"].content_type
+                + ";base64,"
+                + str(base64.b64encode(request.files["foto"].read()), "utf-8")
+            )
+            # monta o JSON para envio a API
+            payload = {"id_produto": id_produto, "nome": nome, "descricao": descricao, "foto": foto, "valor_unitario": valor_unitario}
+        else: 
+            foto = request.form["foto-saved"]
+            # monta o JSON para envio a API
+            payload = {"id_produto": id_produto, "nome": nome, "descricao": descricao,"foto": foto, "valor_unitario": valor_unitario}
+            
         # executa o verbo PUT da API e armazena seu retorno
-        response = requests.put(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API, json=payload)
+        response = requests.put(
+            ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API, json=payload
+        )
         result = response.json()
-        if (response.status_code != 200 or result[1] != 200):
+        if response.status_code != 200 or result[1] != 200:
             raise Exception(result[0])
-        return redirect(url_for('produto.formListaProduto', msg=result[0]))
+        return redirect(url_for("produto.formListaProduto", msg=result[0]))
+    
+    except KeyError as e:
+        return render_template('formListaProduto.html', msgErro=f'Chave ausente: {foto}')
+    except ValueError as e:
+        return render_template('formListaProduto.html', msgErro=f'Valor inválido: {e}')
     except Exception as e:
-        return render_template('formListaProduto.html', msgErro=e.args[0])
+        return render_template('formListaProduto.html', msgErro=str(e))
+
+@bp_produto.route("/delete", methods=["POST"])
+def delete():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form["id_produto"]
+        # executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_PRODUTO + id_produto, headers=HEADERS_API)
+        result = response.json()
+        if response.status_code != 200 or result[1] != 200:
+            raise Exception(result[0])
+        # return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        return jsonify(erro=False, msg=result[0])
+    except Exception as e:
+        # return render_template('formListaFuncionario.html',
+        return jsonify(erro=True, msgErro=e.args[0])
 
 
-'''
+"""
 Rota antiga de app...
 @app.route('/produto/')
 def formListaProduto():
     # return "<h1>Rota da página de Funcionários da nossa WEB APP</h1>", 200
     return render_template('formListaProduto.html'), 200
-'''
+"""
