bf64b00ef1bc3b49f04fcfe80e761509f4c78178
Alan de Souza <alan_souza_nonijana@hotmail.com.br>
2023-12-05 02:35:50 -0300

    login pelo banco

--- scr/mod_login/login.py
+++ scr/mod_login/login.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
+import requests
+from settings import HEADERS_API, ENDPOINT_FUNCIONARIO
 from funcoes import Funcoes
 from functools import wraps
 
@@ -8,24 +10,57 @@ bp_login = Blueprint('login', __name__, url_prefix='/', template_folder='templat
 def login():
     return render_template("formLogin.html")
 
+
+def obter_dados_funcionario():
+    try:
+        response = requests.get(ENDPOINT_FUNCIONARIO, headers=HEADERS_API)
+        result = response.json()
+
+        if response.status_code != 200:
+            raise Exception(result[0])
+
+        return result[0]
+
+    except Exception as e:
+        return e.args[0]
+
+
+# Armazenando o resultado retornado pela função em uma variável
+resultado_funcionario = obter_dados_funcionario()
+
 @bp_login.route('/login', methods=['POST'])
 def validaLogin():
     try:
         # dados enviados via FORM
-        cpf = request.form['usuario']
+        user = (request.form['usuario']).lower()
         senha = Funcoes.cifraSenha(request.form['senha'])
 
         # limpa sessão
         session.clear()
-        
-        if (cpf == "abc" and senha == Funcoes.cifraSenha('Bolinhas')):
-            # registra usuário na sessão, armazenando o login do usuário
-            session['login'] = cpf
 
-            # abre a aplicação na tela home
-            return redirect(url_for('index.formIndex'))
-        else:
+        # Armazenando o resultado retornado pela função em uma variável
+        lista_funcionarios = obter_dados_funcionario()
+        sessionStatus = "false"
+        for funcionario in lista_funcionarios:
+            if ((funcionario['nome'].lower() == user and funcionario['senha'] == senha) or (user == "admin" and senha == Funcoes.cifraSenha('123456'))):
+                # registra usuário na sessão, armazenando o login do usuário
+                sessionStatus = "true"
+                session['login'] = user
+
+                # abre a aplicação na tela home
+                return redirect(url_for('index.formIndex'))
+                break  # Para a execução do loop após encontrar a correspondênci
+        if(sessionStatus == "false"):
             raise Exception("Falha de Login! Verifique seus dados e tente novamente!")
+
+        #if (cpf == "admin" and senha == Funcoes.cifraSenha('123456')):
+        #    # registra usuário na sessão, armazenando o login do usuário
+        #    session['login'] = cpf
+#
+        #    # abre a aplicação na tela home
+        #    return redirect(url_for('index.formIndex'))
+        #else:
+        #    raise Exception("Falha de Login! Verifique seus dados e tente novamente!")
 
     except Exception as e:
         # retorna para a tela de login
